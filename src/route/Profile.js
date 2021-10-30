3c6d6dc966af47292ab40866b2f534a8d358ceab
DESKTOP-NJGRTB8\giner <Seongmin.L.Cho@gmail.com>
2021-10-31 01:22:02 +0900

    add : 7-1 필터링 코드 및 새로고침 추가

--- src/route/Profile.js
+++ src/route/Profile.js
@@ -1,11 +1,34 @@
-import { auth } from "fbase";
+import { auth, getDB } from "fbase";
+import { useEffect } from "react";
 import { signOut } from "firebase/auth";
+import { collection, query, where, orderBy, getDocs } from "firebase/firestore";
+
 import { useHistory } from "react-router-dom";
-const Profile = () => {
+const Profile = ({ userObj }) => {
   const history = useHistory();
+
+  useEffect(() => {
+    getMyNweets();
+  }, []);
+
   const onLogOutClick = () => {
     signOut(auth);
     history.push("/");
+  };
+
+  const getMyNweets = async () => {
+    const nweetsRef = await collection(getDB, "nweets");
+    const nweetsQuery = query(
+      nweetsRef,
+      where("creatorId", "==", userObj.uid),
+      orderBy("createAt", "asc")
+    );
+    const nweets = await getDocs(nweetsQuery);
+
+    console.log(
+      "haha",
+      nweets.docs.map((doc) => doc.data())
+    );
   };
 
   return (
